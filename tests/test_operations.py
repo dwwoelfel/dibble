0fd3837a7277c3f2969831105002af04349d5a87
Sebastian Friedel <sef@voxelbrain.com>
2011-05-12 18:10:41 +0200

    added test for attempting to run an operation on an uninitialized field

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from nose.tools import raises
+import dibble.fields
 import dibble.model
 
 class TestModel(dibble.model.Model):
@@ -19,3 +21,12 @@ def test_increment():
 
     assert dict(m.update) == {'$inc': {'counter': 1}}
     assert m.counter.value == 2
+
+
+@raises(TypeError)
+def test_increment_unset():
+    class UnsetTestModel(dibble.model.Model):
+        counter = dibble.fields.Field()
+
+    m = UnsetTestModel()
+    m.counter.inc(1)
