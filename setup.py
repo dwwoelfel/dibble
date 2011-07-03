6fa074eb52ef27652aa2316e4917d4d0c21d7de2
Sebastian Friedel <sef@voxelbrain.com>
2011-07-04 00:42:58 +0200

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='Dibble',
+      version='0.1',
+      description='Mongodb Object Mapper',
+      url='https://github.com/voxelbrain/dibble',
+      packages=['dibble'],
+     )
